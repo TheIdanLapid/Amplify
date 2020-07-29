89c49deff64ef764fa9a2895df68886d46738398
TheIdanLapid <eontorch@gmail.com>
2020-07-29 17:25:13 +0300

    Initial commit

---
+++ amplify/backend/function/awsb5894603/src/main/java/example/LambdaRequestHandler.java
@@ -0,0 +1,16 @@
+/* Amplify Params - DO NOT EDIT
+	
+Amplify Params - DO NOT EDIT */
+
+package example;
+
+import com.amazonaws.services.lambda.runtime.Context; 
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+public class LambdaRequestHandler implements RequestHandler<RequestClass, ResponseClass>{   
+
+    public ResponseClass handleRequest(RequestClass request, Context context){
+        String greetingString = String.format("Hello %s %s!", request.firstName, request.lastName);
+        return new ResponseClass(greetingString);
+    }
+}
